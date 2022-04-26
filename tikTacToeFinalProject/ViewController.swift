8b4dcc12a67bbb857c3ebb2ebfd0830be00fdd26
Nathan Kim <nkim3215@stu.d214.org>
2022-04-26 12:51:31 -0500

    created ibactions for each button

--- tikTacToeFinalProject/ViewController.swift
+++ tikTacToeFinalProject/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class ViewController: UIViewController
 {
 
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
@@ -17,7 +18,33 @@ class ViewController: UIViewController
         navigationItem.title = "Tik Tac Toe!"
         
     }
-   
-
+    
+    @IBAction func button1 (_ sender: UIButton) {
+        
+    }
+    @IBAction func button2 (_ sender: UIButton) {
+        
+    }
+    @IBAction func button3 (_ sender: UIButton) {
+        
+    }
+    @IBAction func button4 (_ sender: UIButton) {
+        
+    }
+    @IBAction func button5 (_ sender: UIButton) {
+        
+    }
+    @IBAction func button6 (_ sender: UIButton) {
+        
+    }
+    @IBAction func button7 (_ sender: UIButton) {
+        
+    }
+    @IBAction func button8 (_ sender: UIButton) {
+        
+    }
+    @IBAction func button9 (_ sender: UIButton) {
+        
+    }
 }
 
